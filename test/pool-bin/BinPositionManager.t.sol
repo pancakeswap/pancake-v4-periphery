ca93ee7f19dfe9cca80e2bf7e4c5bc2eb4a731fb
ChefMist <133624774+ChefMist@users.noreply.github.com>
2025-01-25 16:52:23 +0800

    feat: remove initcode hash test

--- test/pool-bin/BinPositionManager.t.sol
+++ test/pool-bin/BinPositionManager.t.sol
@@ -46,11 +46,4 @@ contract BinPositionManagerTest is Test, GasSnapshot, DeployPermit2 {
             revert ContractSizeTooLarge(address(binPm).code.length - 24576);
         }
     }
-
-    function test_initcodeHash() public {
-        vm.snapshotValue(
-            "BinPositionManager initcode hash (without constructor params, as uint256)",
-            uint256(keccak256(type(BinPositionManager).creationCode))
-        );
-    }
 }
