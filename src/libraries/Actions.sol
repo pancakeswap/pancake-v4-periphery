5b51a59380404f28846f4e4a3b6d66f35b8ba5ae
ChefMist <133624774+ChefMist@users.noreply.github.com>
2025-01-26 17:45:49 +0800

    s50: use internal keyword to make visibility obvious

--- src/libraries/Actions.sol
+++ src/libraries/Actions.sol
@@ -8,55 +8,55 @@ pragma solidity ^0.8.0;
 library Actions {
     // cl-pool actions
     // liquidity actions
-    uint256 constant CL_INCREASE_LIQUIDITY = 0x00;
-    uint256 constant CL_DECREASE_LIQUIDITY = 0x01;
-    uint256 constant CL_MINT_POSITION = 0x02;
-    uint256 constant CL_BURN_POSITION = 0x03;
-    uint256 constant CL_INCREASE_LIQUIDITY_FROM_DELTAS = 0x04;
-    uint256 constant CL_MINT_POSITION_FROM_DELTAS = 0x05;
+    uint256 internal constant CL_INCREASE_LIQUIDITY = 0x00;
+    uint256 internal constant CL_DECREASE_LIQUIDITY = 0x01;
+    uint256 internal constant CL_MINT_POSITION = 0x02;
+    uint256 internal constant CL_BURN_POSITION = 0x03;
+    uint256 internal constant CL_INCREASE_LIQUIDITY_FROM_DELTAS = 0x04;
+    uint256 internal constant CL_MINT_POSITION_FROM_DELTAS = 0x05;
 
     // swapping
-    uint256 constant CL_SWAP_EXACT_IN_SINGLE = 0x06;
-    uint256 constant CL_SWAP_EXACT_IN = 0x07;
-    uint256 constant CL_SWAP_EXACT_OUT_SINGLE = 0x08;
-    uint256 constant CL_SWAP_EXACT_OUT = 0x09;
+    uint256 internal constant CL_SWAP_EXACT_IN_SINGLE = 0x06;
+    uint256 internal constant CL_SWAP_EXACT_IN = 0x07;
+    uint256 internal constant CL_SWAP_EXACT_OUT_SINGLE = 0x08;
+    uint256 internal constant CL_SWAP_EXACT_OUT = 0x09;
 
     // donate
     /// @dev this is not supported in the position manager or router
-    uint256 constant CL_DONATE = 0x0a;
+    uint256 internal constant CL_DONATE = 0x0a;
 
     // closing deltas on the pool manager
     // settling
-    uint256 constant SETTLE = 0x0b;
-    uint256 constant SETTLE_ALL = 0x0c;
-    uint256 constant SETTLE_PAIR = 0x0d;
+    uint256 internal constant SETTLE = 0x0b;
+    uint256 internal constant SETTLE_ALL = 0x0c;
+    uint256 internal constant SETTLE_PAIR = 0x0d;
     // taking
-    uint256 constant TAKE = 0x0e;
-    uint256 constant TAKE_ALL = 0x0f;
-    uint256 constant TAKE_PORTION = 0x10;
-    uint256 constant TAKE_PAIR = 0x11;
+    uint256 internal constant TAKE = 0x0e;
+    uint256 internal constant TAKE_ALL = 0x0f;
+    uint256 internal constant TAKE_PORTION = 0x10;
+    uint256 internal constant TAKE_PAIR = 0x11;
 
-    uint256 constant CLOSE_CURRENCY = 0x12;
-    uint256 constant CLEAR_OR_TAKE = 0x13;
-    uint256 constant SWEEP = 0x14;
-    uint256 constant WRAP = 0x15;
-    uint256 constant UNWRAP = 0x16;
+    uint256 internal constant CLOSE_CURRENCY = 0x12;
+    uint256 internal constant CLEAR_OR_TAKE = 0x13;
+    uint256 internal constant SWEEP = 0x14;
+    uint256 internal constant WRAP = 0x15;
+    uint256 internal constant UNWRAP = 0x16;
 
     // minting/burning 6909s to close deltas
     /// @dev this is not supported in the position manager or router
-    uint256 constant MINT_6909 = 0x17;
-    uint256 constant BURN_6909 = 0x18;
+    uint256 internal constant MINT_6909 = 0x17;
+    uint256 internal constant BURN_6909 = 0x18;
 
     // bin-pool actions
     // liquidity actions
-    uint256 constant BIN_ADD_LIQUIDITY = 0x19;
-    uint256 constant BIN_REMOVE_LIQUIDITY = 0x1a;
-    uint256 constant BIN_ADD_LIQUIDITY_FROM_DELTAS = 0x1b;
+    uint256 internal constant BIN_ADD_LIQUIDITY = 0x19;
+    uint256 internal constant BIN_REMOVE_LIQUIDITY = 0x1a;
+    uint256 internal constant BIN_ADD_LIQUIDITY_FROM_DELTAS = 0x1b;
     // swapping
-    uint256 constant BIN_SWAP_EXACT_IN_SINGLE = 0x1c;
-    uint256 constant BIN_SWAP_EXACT_IN = 0x1d;
-    uint256 constant BIN_SWAP_EXACT_OUT_SINGLE = 0x1e;
-    uint256 constant BIN_SWAP_EXACT_OUT = 0x1f;
+    uint256 internal constant BIN_SWAP_EXACT_IN_SINGLE = 0x1c;
+    uint256 internal constant BIN_SWAP_EXACT_IN = 0x1d;
+    uint256 internal constant BIN_SWAP_EXACT_OUT_SINGLE = 0x1e;
+    uint256 internal constant BIN_SWAP_EXACT_OUT = 0x1f;
     // donate
-    uint256 constant BIN_DONATE = 0x20;
+    uint256 internal constant BIN_DONATE = 0x20;
 }
